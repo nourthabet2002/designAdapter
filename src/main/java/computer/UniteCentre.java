2fd81815cbc82bf04d9464de801f990272886ed7
nourthabet2002 <nour2OO2thabet@gmail.com>
2025-11-25 17:06:46 +0100

    Initial commit

---
+++ src/main/java/computer/UniteCentre.java
@@ -0,0 +1,16 @@
+package computer;
+
+public class UniteCentre {
+    private Vga vga;
+
+    public void print(String message) {
+        System.out.println("********************");
+        vga.print(message);
+        System.out.println("********************");
+
+    }
+
+    public void setVga(Vga vga) {
+        this.vga = vga;
+    }
+}
